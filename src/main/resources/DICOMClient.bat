bddd4af96f8c79914f95c5a06e8f6fabadbb1e75
irrer <irrer@med.umich.edu>
2012-10-26 19:29:45 -0400

    Fixed logging problem

--- src/main/resources/DICOMClient.bat
+++ src/main/resources/DICOMClient.bat
@@ -11,4 +11,8 @@ set PATH=%JAVA_HOME%\bin;%PATH%
 @rem set the current directory so that logs will go in the right place
 cd %dirname%
 
-java -Xmx256m -cp %dirname%@@with-dep-jar@@ -Djava.util.logging.config.file=%dirname%logging.propertiesWindows edu.umro.dicom.client.DicomClient %*
+if exist log goto gotlog
+    mkdir log
+:gotlog
+
+java -Xmx256m -cp %dirname%@@with-dep-jar@@ -Djava.util.logging.config.file=%dirname%logging.propertiesWindows edu.umro.dicom.client.DicomClient %* > log\%DATE:~10,4%_%DATE:~4,2%_%DATE:~7,2%_%TIME:~0,2%_%TIME:~3,2%_%TIME:~6,2%_%TIME:~9,2%.log 2>&1
