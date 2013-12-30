4af3559cc7d9cf1b05bda1f3a5060ae40f24c7e0
Jim Irrer <irrer@umich.edu>
2013-12-30 10:57:24 -0500

    reorganized HTML files

--- user_manual/user_manual_1.0.28/build.sh
+++ user_manual/user_manual_1.0.28/build.sh
@@ -1,9 +1,9 @@
 
 mkdir target >/dev/null 2>&1
 
-for f in *Content.html ; do
-	out=target/$( echo $f | sed 's/Content.html/.html/')
-	echo Constructing $out ...
-    cat head.html $f tail.html > $out
+for f in *.html ; do
+	out=target/${f}
+	echo Constructing ${out} ...
+    cat common/head.html ${f} common/tail.html > ${out}
 done
 
