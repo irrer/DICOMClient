0b0b597173a05666ebb7f48bdeeeef1866f8d623
Jim Irrer <irrer@umich.edu>
2014-06-17 16:31:36 -0400

    Increased memory limit.

--- src/main/resources/DICOMClient.bat
+++ src/main/resources/DICOMClient.bat
@@ -27,4 +27,4 @@ if exist log goto gotlog
     mkdir log
 :gotlog
 
-java -Xmx256m -cp %dirname%@@with-dep-jar@@ -Djava.util.logging.config.file=%dirname%logging.propertiesWindows edu.umro.dicom.client.DicomClient %* > log\%DATE:~10,4%_%DATE:~4,2%_%DATE:~7,2%_%TIME:~0,2%_%TIME:~3,2%_%TIME:~6,2%_%TIME:~9,2%.log 2>&1
+java -Xmx4096m -cp %dirname%@@with-dep-jar@@ -Djava.util.logging.config.file=%dirname%logging.propertiesWindows edu.umro.dicom.client.DicomClient %* > log\%DATE:~10,4%_%DATE:~4,2%_%DATE:~7,2%_%TIME:~0,2%_%TIME:~3,2%_%TIME:~6,2%_%TIME:~9,2%.log 2>&1
