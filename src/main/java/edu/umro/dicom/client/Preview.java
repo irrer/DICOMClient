ae7951166a31d022a68b9978599b8a5880b6a766
Jim Irrer <irrer@umich.edu>
2022-06-29 17:37:54 -0400

    Removed dependency on java package that prevented the use of newer JVMs.
    The following package was no longer available in newer JVMs (such as Java 17 and 18):
      com.sun.java.swing.plaf.windows.WindowsSliderUI
    
    Using this package allowed the user to increment a slider by one increment by clicking to either side of it.  The same functionality can be obtained by using the right and left arrow keyboard keys.

--- src/main/java/edu/umro/dicom/client/Preview.java
+++ src/main/java/edu/umro/dicom/client/Preview.java
@@ -542,10 +542,10 @@ public class Preview implements ActionListener, ChangeListener, DocumentListener
         contrastSlider.setMinorTickSpacing(1);
         contrastSlider.setPaintTicks(true);
         contrastSlider.addChangeListener(this);
-        if (UIManager.getLookAndFeel().getID().equalsIgnoreCase("Windows")) {
-            SliderUI sliderUI = new WinSliderUI(contrastSlider);
-            contrastSlider.setUI(sliderUI);
-        }
+//         if (UIManager.getLookAndFeel().getID().equalsIgnoreCase("Windows")) {
+//             SliderUI sliderUI = new WinSliderUI(contrastSlider);
+//             contrastSlider.setUI(sliderUI);
+//         }
         JPanel contrastPanel = new JPanel();
         contrastPanel.setLayout(new BorderLayout());
         contrastPanel.add(new JLabel("    Contrast"), BorderLayout.NORTH);
@@ -558,10 +558,10 @@ public class Preview implements ActionListener, ChangeListener, DocumentListener
         brightnessSlider.setMinorTickSpacing(8);
         brightnessSlider.setPaintTicks(true);
         brightnessSlider.addChangeListener(this);
-        if (UIManager.getLookAndFeel().getID().equalsIgnoreCase("Windows")) {
-            SliderUI sliderUI = new WinSliderUI(brightnessSlider);
-            brightnessSlider.setUI(sliderUI);
-        }
+//         if (UIManager.getLookAndFeel().getID().equalsIgnoreCase("Windows")) {
+//             SliderUI sliderUI = new WinSliderUI(brightnessSlider);
+//             brightnessSlider.setUI(sliderUI);
+//         }
         JPanel brightnessPanel = new JPanel();
         brightnessPanel.setLayout(new BorderLayout());
         brightnessPanel.add(new JLabel("    Brightness"), BorderLayout.NORTH);
@@ -574,10 +574,10 @@ public class Preview implements ActionListener, ChangeListener, DocumentListener
         zoomSlider.setMinorTickSpacing(1);
         zoomSlider.setPaintTicks(true);
         zoomSlider.addChangeListener(this);
-        if (UIManager.getLookAndFeel().getID().equalsIgnoreCase("Windows")) {
-            SliderUI sliderUI = new WinSliderUI(zoomSlider);
-            zoomSlider.setUI(sliderUI);
-        }
+//         if (UIManager.getLookAndFeel().getID().equalsIgnoreCase("Windows")) {
+//             SliderUI sliderUI = new WinSliderUI(zoomSlider);
+//             zoomSlider.setUI(sliderUI);
+//         }
         JPanel zoomPanel = new JPanel();
         zoomPanel.setLayout(new BorderLayout());
         zoomPanel.add(new JLabel("    Zoom"), BorderLayout.NORTH);
@@ -672,10 +672,12 @@ public class Preview implements ActionListener, ChangeListener, DocumentListener
         sliceSlider.setMinorTickSpacing(1);
         sliceSlider.setPaintTicks(true);
         sliceSlider.addChangeListener(this);
+        /*
         if (UIManager.getLookAndFeel().getID().equalsIgnoreCase("Windows")) {
             SliderUI sliderUI = new WinSliderUI(sliceSlider);
             sliceSlider.setUI(sliderUI);
         }
+        */
 
         panel.add(sliceSlider, BorderLayout.CENTER);
 
--- src/main/java/edu/umro/dicom/client/WinSliderUI.java
+++ src/main/java/edu/umro/dicom/client/WinSliderUI.java
@@ -16,41 +16,44 @@ package edu.umro.dicom.client;
  * limitations under the License.
  */
 
-import javax.swing.JSlider;
-import com.sun.java.swing.plaf.windows.WindowsSliderUI;
+// import javax.swing.JSlider;
+// import com.sun.java.swing.plaf.windows.WindowsSliderUI;
+//
+// /**
+//  * Override the standard Windows slider by incrementing
+//  * a slider by one instead of roughly 10% of the range.
+//  *
+//  * This give the user finer control over positioning sliders.
+//  *
+//  * @author Jim Irrer  irrer@umich.edu
+//  *
+//  */
+// @SuppressWarnings("restriction")
+// public class WinSliderUI extends WindowsSliderUI {
+//
+//     /**
+//      * Use the same constructor.
+//      *
+//      * @param jSlider Slider.
+//      */
+//     public WinSliderUI(JSlider jSlider) {
+//         super(jSlider);
+//     }
+//
+//     /**
+//      * Increment the slider by 1 when the user clicks on
+//      * either side of the marker.
+//      */
+//     public void scrollByBlock(int direction)    {
+//         synchronized(slider) {
+//
+//             int oldValue = slider.getValue();
+//             int blockIncrement = 1;
+//             int delta = blockIncrement * ((direction > 0) ? POSITIVE_SCROLL : NEGATIVE_SCROLL);
+//             slider.setValue(oldValue + delta);
+//         }
+//     }
+// }
 
-/**
- * Override the standard Windows slider by incrementing
- * a slider by one instead of roughly 10% of the range.
- * 
- * This give the user finer control over positioning sliders.
- * 
- * @author Jim Irrer  irrer@umich.edu 
- *
- */
-@SuppressWarnings("restriction")
-public class WinSliderUI extends WindowsSliderUI {
-
-    /**
-     * Use the same constructor.
-     * 
-     * @param jSlider Slider.
-     */
-    public WinSliderUI(JSlider jSlider) {
-        super(jSlider);
-    }
-
-    /**
-     * Increment the slider by 1 when the user clicks on
-     * either side of the marker.
-     */
-    public void scrollByBlock(int direction)    {
-        synchronized(slider) {
-
-            int oldValue = slider.getValue();
-            int blockIncrement = 1;
-            int delta = blockIncrement * ((direction > 0) ? POSITIVE_SCROLL : NEGATIVE_SCROLL);
-            slider.setValue(oldValue + delta);          
-        }
-    }
+public class WinSliderUI {
 }
